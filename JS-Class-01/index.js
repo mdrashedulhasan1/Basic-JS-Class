ffbe6e7793dc27956d24f6cd4df80b5073966aa2
Md. Rashedul Hasan <76987245+mdrashedulhasan1@users.noreply.github.com>
2022-05-25 21:47:13 +0600

    Add files via upload
    Basic JS Class

---
+++ JS-Class-01/index.js
@@ -0,0 +1,50 @@
+// console.log("First Class");
+
+// var mangoPrice = 10;
+// console.log(mangoPrice);
+
+// var mangoPrice = 10;
+// console.log(typeof mangoPrice);
+
+// var mangoPrice = 10.578;
+// console.log(typeof mangoPrice);
+
+// var myName = "Tapu";
+// console.log(typeof myName);
+
+// var isGood = true;
+// console.log(typeof isGood);
+
+// var isBad = false;
+// console.log(typeof isBad);
+
+
+// var myCountry = "I love Bangladesh";
+// var lowerCase = myCountry.toLowerCase();
+// console.log(lowerCase);
+// var upperCase = myCountry.toUpperCase();
+// console.log(upperCase);
+// var position = myCountry.indexOf("love");
+// console.log(position);
+// var split = myCountry.split(" ");
+// console.log(split);
+
+// var num = 10;
+// var num2 = parseFloat('5.75');
+// var total = num+num2;
+// console.log(total);
+
+// var num1 = 10;
+// var num2 = 3;
+// var add = num1+num2;
+// var sub = num1-num2;
+// var multi = num1*num2;
+// var div = (num1/num2).toFixed(2);
+// var div = parseFloat(div);
+// var rem = num1%num2;
+// console.log(add,sub,multi,div,rem);
+
+var firstName = "Rashedul";
+var lastName = "Hasan";
+var fullName = firstName+" "+lastName;
+console.log(fullName);
---
+++ JS-Class-02/index.js
@@ -0,0 +1,38 @@
+// var num = Math.abs(-2);
+// console.log(num);
+
+// var num = Math.ceil(5.57);
+// console.log(num); //6
+
+// var num = Math.floor(5.65767);
+// console.log(num);
+
+// var num = Math.round(5.56776);
+// console.log(num); //6
+
+// var num = Math.round(5.4999);
+// console.log(num); //5
+
+// var num = Math.random()*10;
+// var num = Math.round(num);
+// console.log(num);
+
+// var mangoPrice = 20;
+// if(mangoPrice != 20){
+//     console.log('Buy Mango');
+// }
+// else{
+//     console.log("Can't Buy Mango");
+// }
+
+var friend1Taka = 100;
+var friend2Taka = 50;
+if(friend1Taka == 199 && friend2Taka == 50){
+    console.log("Go to Restaturant-01");
+}
+else if(friend1Taka >= 100 || friend2Taka >= 100){
+    console.log("Go to Restaturant-02");
+}
+else{
+    console.log("Can't go to Restaturant"); 
+}
